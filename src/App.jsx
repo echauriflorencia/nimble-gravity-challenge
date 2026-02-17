2e8e334d11daecbc6d46362c20ab84bef154d647
echauriflorencia <florenciaechauri@gmail.com>
2026-02-17 17:36:34 -0300

    feat: fetch candidate and jobs from API with basic error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
+const BASE_URL = "https://botfilter-h5ddh6dye8exb7ha.centralus-01.azurewebsites.net/";
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [candidate, setCandidate] = useState(null);
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchCandidate = async () => {
+      try {
+        const response = await fetch(
+          `${BASE_URL}api/candidate/get-by-email?email=florenciaechauri@gmail.com`
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error - Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setCandidate(data);
+      } catch (error) {
+        console.error("Error fetching candidate:", error.message);
+      }
+    };
+
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch(
+          `${BASE_URL}api/jobs/get-list`
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error - Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setJobs(data);
+      } catch (error) {
+        console.error("Error fetching jobs: ", error.message);
+      }
+    };
+
+    fetchCandidate();
+    fetchJobs();
+  }, []);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {jobs.map(job => (
+        <div key={job.id}>
+          <h3>{job.title}</h3>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
